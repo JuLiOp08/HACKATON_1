bfed3eda3d26fd992b0be3c8084eb1592333e906
Yeimi Varela <yeimi.varela@utec.edu.pe>
2025-05-06 16:58:55 -0500

    ModeloIA repository creado

---
+++ src/main/java/com/example/pc_piatto/Repositories/ModeloIARepository.java
@@ -0,0 +1,8 @@
+package com.example.pc_piatto.Repositories;
+
+import com.example.pc_piatto.domain.ModeloIA;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModeloIARepository extends JpaRepository<ModeloIA, Long> {
+    boolean existsByNombreAndProveedor(String nombre, String proveedor);
+}
