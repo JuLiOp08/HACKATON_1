4e0cb305a91868a643f7d3ce6a19f238439d1c98
Yeimi Varela <yeimi.varela@utec.edu.pe>
2025-05-06 18:03:45 -0500

    LimiteUsuarioModeloController creado

---
+++ src/main/java/com/example/pc_piatto/application/LimiteUsuarioModeloController .java
@@ -0,0 +1,20 @@
+package com.example.pc_piatto.application;
+
+import com.example.pc_piatto.application.LimiteUsuarioModeloService;
+import com.example.pc_piatto.dto.LimiteUsuarioModeloDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/company/users")
+public class LimiteUsuarioModeloController {
+
+    @Autowired
+    private LimiteUsuarioModeloService limiteService;
+
+    @PostMapping("/{id}/limits")
+    public LimiteUsuarioModeloDTO asignarLimite(@PathVariable Long id,
+                                                @RequestBody LimiteUsuarioModeloDTO dto) {
+        return limiteService.asignarLimite(id, dto);
+    }
+}
