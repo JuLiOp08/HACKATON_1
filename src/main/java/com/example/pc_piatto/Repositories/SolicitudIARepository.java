e4c0bed7885eb97fbcd1e755da85977a6341cceb
JuLiOp08 <julio.portugal@utec.edu.pe>
2025-05-06 17:03:21 -0500

    Creando el SolicitudIARepository

---
+++ src/main/java/com/example/pc_piatto/Repositories/SolicitudIARepository.java
@@ -0,0 +1,13 @@
+package com.example.pc_piatto.Repositories;
+
+import com.example.pc_piatto.domain.SolicitudIA;
+import com.example.pc_piatto.domain.Usuario;
+import com.example.pc_piatto.domain.ModeloIA;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SolicitudIARepository extends JpaRepository<SolicitudIA, Long> {
+    List<SolicitudIA> findByUsuario(Usuario usuario);
+    List<SolicitudIA> findByModelo(ModeloIA modelo);
+}
