1a69244d5c8686f53e66683560435ff40927504d
Yeimi Varela <yeimi.varela@utec.edu.pe>
2025-05-07 12:03:39 -0500

    Endpoints elaborados para la Gestión de Usuarios y Límites

--- src/main/java/com/example/pc_piatto/application/LimiteUsuarioModeloController.java
+++ src/main/java/com/example/pc_piatto/application/LimiteUsuarioModeloController.java
@@ -2,8 +2,13 @@ package com.example.pc_piatto.application;
 
 import com.example.pc_piatto.domain.LimiteUsuarioModeloService;
 import com.example.pc_piatto.dto.LimiteUsuarioModeloDTO;
+import com.example.pc_piatto.dto.UsuarioDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import com.example.pc_piatto.domain.UsuarioService;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/company/users")
@@ -17,12 +22,29 @@ public class LimiteUsuarioModeloController {
         return limiteService.crear(dto);
     }
 
+    @GetMapping
+    public List<UsuarioDTO> listar() {
+        return UsuarioService.listarUsuarios();
+    }
+
+    @GetMapping("/{id}")
+    public UsuarioDTO obtener(@PathVariable Long id) {
+        return UsuarioService.obtenerUsuario(id);
+    }
+
+    @PutMapping("/{id}")
+    public UsuarioDTO actualizar(@PathVariable Long id, @RequestBody UsuarioDTO dto) {
+        return UsuarioService.actualizarUsuario(id, dto);
+    }
 
     @PostMapping("/{id}/limits")
-    public LimiteUsuarioModeloDTO asignarLimite(@PathVariable Long id,
-                                                @RequestBody LimiteUsuarioModeloDTO dto) {
+    public LimiteUsuarioModeloDTO asignarLimite(@PathVariable Long id, @RequestBody LimiteUsuarioModeloDTO dto) {
         return limiteService.asignarLimite(id, dto);
     }
 
+    @GetMapping("{id}/consumption")
+    public LimiteUsuarioModeloDTO obtenerLimite(@PathVariable Long id) {
+        return UsuarioService.obtenerConsumo(id);
+    }
 
 }
