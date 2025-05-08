adccef9b8c6d0a3f000a79db48dd8077d2c5c327
JuLiOp08 <julio.portugal@utec.edu.pe>
2025-05-07 21:10:06 -0500

    Solucionando errores en usuario

--- src/main/java/com/example/pc_piatto/repository/UsuarioRepository.java
+++ src/main/java/com/example/pc_piatto/repository/UsuarioRepository.java
@@ -6,6 +6,9 @@ import com.example.pc_piatto.domain.RolUsuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    List<Usuario> findByEmpresaId(Long empresaId);
+    Optional<Usuario> findByCorreo(String correo);
 }
